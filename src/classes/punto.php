8d926136df04faee12d6d128039ea74c555d9f93
José Antonio <joseapaez96@gmail.com>
2022-05-17 16:51:15 +0200

    Segundo método de punto.php

--- src/classes/punto.php
+++ src/classes/punto.php
@@ -10,7 +10,7 @@ class punto{
         $this-> positiony = $positiony;
     }
 
-    public function getPunto(int $positionx, int $positiony):punto{
+    public function getPunto(int $positionx, int $positiony){
         return new punto($positionx, $positiony); 
     }
     //+= establece el valor de la variable a otro
@@ -19,9 +19,27 @@ class punto{
         $this-> positiony += $positiony;
     }
 
-    public function moveposition(){
-        $movex
-        $movey
+    public function moveposition($positionx, $positiony, $posicion){
+        $movex = 0;
+        $movey = 0;
+        $posicion = [$positionx, $positiony];
+        $newposicion = [];
+        $this-> positionx = $positionx;
+        $this-> positiony = $positiony;
+        for($positionx=0; $positionx<$movex; $positionx++){ 
+            for($positiony=0; $positiony<$movey; $positiony++){
+                $newposicion == $posicion[$positionx][$positiony] = $posicion[$positionx++][$positiony++];
+            }
+            return $newposicion;
+        }
+    
+        for($ix=0; $ix<11; $ix++){ //Lo de abajo
+            for($iy=0; $iy<11; $iy++){ //Queremos que nos de todas las tablas de multiplicar del 1 al 10
+                $array[$ix][$iy]=$ix*$iy; //Con los for anteriores,
+                                          // se va incrementando uno por uno cada multiplicación
+            }
+        }
+        return $array;
     }
         
     public function toString(){
