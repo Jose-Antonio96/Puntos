b1700bd85654f8d9c72685e1d8cb27f3ae260ff3
Jose Antonio <joseapaez96@gmail.com>
2022-05-18 11:30:07 +0200

    main

--- src/classes/poligono.php
+++ src/classes/poligono.php
@@ -2,11 +2,9 @@
 namespace ITEC\PRESENCIAL\DAW\PROG\puntos;
 use ITEC\PRESENCIAL\DAW\PROG\puntos\punto;
 class poligono{
-    private area $area;
-    private lado $lado;
-
-     
 
     public function calcularArea(){
     }
 }
+
+?>
