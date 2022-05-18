b527926557425279afed71a9443a7cb753c139b6
Jose Antonio <joseapaez96@gmail.com>
2022-05-18 11:56:18 +0200

    main

--- src/classes/circulo.php
+++ src/classes/circulo.php
@@ -3,17 +3,13 @@ namespace ITEC\PRESENCIAL\DAW\PROG\puntos;
 use ITEC\PRESENCIAL\DAW\PROG\puntos\punto;
 use ITEC\PRESENCIAL\DAW\PROG\puntos\poligono;
 
-class cuadrado extends poligono{
+class circulo extends poligono{
     private punto $A;
     private punto $B; 
-    private punto $C;
-    private punto $D;
 
-    public function __construct(punto $A, punto $B, punto $C, punto $D){
+    public function __construct(punto $A, punto $B){
         $this-> A = $A;
         $this-> B = $B;
-        $this-> C = $C;
-        $this-> D = $D;
     }
 
     public function calcularArea()
